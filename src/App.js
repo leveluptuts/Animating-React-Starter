306a2fbc819a47b785920b37ac4d2e7624aa80a2
Scott Tolinski <scott.tolinski@gmail.com>
2019-04-12 09:58:37 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,18 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="logo" />
+          <button className="menu-button">Menu</button>
         </header>
       </div>
     );
